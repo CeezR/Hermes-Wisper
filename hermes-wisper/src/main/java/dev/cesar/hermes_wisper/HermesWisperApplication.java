fe2514e885c956e41b60e5749971d65865e4d380
CeezR <cesarforthomme@gmail.com>
2025-03-25 20:07:13 +0100

    chore: project initialization

---
+++ hermes-wisper/src/main/java/dev/cesar/hermes_wisper/HermesWisperApplication.java
@@ -0,0 +1,13 @@
+package dev.cesar.hermes_wisper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesWisperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermesWisperApplication.class, args);
+	}
+
+}
---
+++ hermes-wisper/src/test/java/dev/cesar/hermes_wisper/HermesWisperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cesar.hermes_wisper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesWisperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
