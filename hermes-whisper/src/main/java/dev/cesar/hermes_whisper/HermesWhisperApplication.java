b3763a5a847b963936cd8d76b06fb2683551f4f9
CeezR <cesarforthomme@gmail.com>
2025-03-26 08:24:10 +0100

    feat: dockerfile added and name fix

---
+++ hermes-whisper/src/main/java/dev/cesar/hermes_whisper/HermesWhisperApplication.java
@@ -0,0 +1,13 @@
+package dev.cesar.hermes_whisper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesWhisperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermesWhisperApplication.class, args);
+	}
+
+}
---
+++ hermes-whisper/src/test/java/dev/cesar/hermes_whisper/HermesWhisperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cesar.hermes_whisper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesWhisperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
